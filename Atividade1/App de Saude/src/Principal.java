330e827785c35f7462803127f45c61aa1137337b
Geofpr <geovannarpacifico@gmail.com>
2026-04-08 08:40:36 -0300

    Delete Atividade1 directory

--- Atividade1/App de Saude/src/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        AtividadeFisica caminhada = new AtividadeFisica(30, 2);
-        Corrida corrida = new Corrida(30, 2, 5);
-
-        System.out.println("Calorias da atividade física: " + caminhada.calcularCalorias());
-        System.out.println("Calorias da corrida: " + corrida.calcularCalorias());
-    }
-}
